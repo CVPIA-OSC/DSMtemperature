be035b08087780ae3efca72ea960ae8543f4202f
ErinCain <ecain@flowwest.com>
2021-02-24 10:53:24 -0800

    Adds code to make switching between NOAA lodi and antioch data easy so we can change after we choose what gauge to use

--- data-raw/deltas/ges_north_delta_temp_modeling.R
+++ data-raw/deltas/ges_north_delta_temp_modeling.R
@@ -100,30 +100,30 @@ water_temp_training %>%
   geom_point() +
   geom_smooth(method = 'lm', se = FALSE)
 
-# Create Lodi GES Training
+# Create GES Training
 GES_north_delta_training <- water_temp_training %>%
   left_join(air_temp_training) %>%
   filter(!is.na(air_temp_c))
 
 
-# model for lodi
+# model for GES
 GES_north_delta_temp_model <- lm(water_temp_c ~ air_temp_c, GES_north_delta_training)
 summary(GES_north_delta_temp_model)
 
 # Lodi temp data
-# GES_north_delta_air_temp <- lodi1$data %>%
-#   bind_rows(lodi2$data) %>%
-#   bind_rows(lodi3$data) %>%
-#   mutate(date = as_date(ymd_hms(date))) %>%
-#   select(date, air_temp_c = value) %>%
-#   bind_rows(
-#     tibble(date = seq.Date(ymd('1979-01-01'), ymd('2000-12-01'), by = 'month'),
-#            air_temp_c = 0)
-#   ) %>%
-#   group_by(date) %>%
-#   summarise(air_temp_c = max(air_temp_c)) %>%
-#   ungroup() %>%
-#   mutate(air_temp_c = ifelse(air_temp_c == 0, NA, air_temp_c))
+GES_north_delta_air_temp <- lodi1$data %>%
+  bind_rows(lodi2$data) %>%
+  bind_rows(lodi3$data) %>%
+  mutate(date = as_date(ymd_hms(date))) %>%
+  select(date, air_temp_c = value) %>%
+  bind_rows(
+    tibble(date = seq.Date(ymd('1979-01-01'), ymd('2000-12-01'), by = 'month'),
+           air_temp_c = 0)
+  ) %>%
+  group_by(date) %>%
+  summarise(air_temp_c = max(air_temp_c)) %>%
+  ungroup() %>%
+  mutate(air_temp_c = ifelse(air_temp_c == 0, NA, air_temp_c))
 
 # antioch temp data
 # GES_north_delta_air_temp <- antioch1$data %>%
